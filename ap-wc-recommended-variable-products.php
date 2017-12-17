3f49cbe5256d5a2570d94d66c3a3cd1186435031
asp111 <ashishpatil111@gmail.com>
2017-12-17 12:17:06 +0000

    Initial setup of Plugin name, version, license

---
+++ ap-wc-recommended-variable-products.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: Woocommerce Recommended Variable Products 
+ * Plugin URI: http://ashishpatil.me
+ * Description: This plugins add recommended/cross sell/ or custom ID's products variation on the single product page
+ * Version: 1.0.0
+ * Author: Ashish Patil
+ * Author URI: http://ashishpatil.me
+ * License: MIT License
+ */
+
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
